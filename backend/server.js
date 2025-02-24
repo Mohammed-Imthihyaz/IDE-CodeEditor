fe759435710d65be83e54e1cbafdc8a93b1b80c9
Mohammed-Imthihyaz <141853784+Mohammed-Imthihyaz@users.noreply.github.com>
2025-02-24 22:32:06 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const app = express();
 const PORT = process.env.PORT|| 5001;
 
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 
 // Use middleware to parse JSON
 app.use(express.json()); // Corrected to use parentheses
